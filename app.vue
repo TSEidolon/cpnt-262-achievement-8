f2b46bac6ee5b49b025ed31df2849db7bf73277c
Edgar Francis Caballero <efrcaballero@gmail.com>
2022-03-29 19:55:53 -0600

    Initial setup

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <header>
+      <h1> {{ zelda.data.monsters[0].name }} </h1>
+      <img :src ="zelda.data.monsters[0].image" alt="">
+      <p> {{ zelda.data.monsters[0].description }} </p>
+      
+      <p> Drops: </p>
+      <ul>
+        <li v-for="x in zelda.data.monsters[0].drops" :key="x">
+          {{x}}
+        </li>
+      </ul>
+
+      <p> Common Locations: </p>
+      <ul>
+        <li v-for="x in zelda.data.monsters[0].common_locations" :key="x">
+          {{x}}
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script setup>
+  const { data: zelda } = await useFetch('https://botw-compendium.herokuapp.com/api/v2')
+  console.log (zelda.value.data.monsters[0])
+</script>
